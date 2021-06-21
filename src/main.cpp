e2007c331999208373d6d6e522f3687557522818
환석 <ghks9705@gmail.com>
2021-06-21 13:57:33 +0000

    2트.....

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,81 @@
+// 검은 색이 true, 대문자, 위 쪽
 // 할 것
-// 1. 말의 움직임을 어떻게 저장 할 것인가?
-// 2. 게임 진행을 어떻게 할 것인가
+// 말의 움직임을 어떻게 저장 할 것인가?
+// 보드한칸에 이니셜로 무슨 말인지 알아내기
 
+/*
+	0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7
+	1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7
+	2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7
+	3.0 3.1 3.2 3.3 3.4 3.5 3.6 3.7
+	4.0 4.1 4.2 4.3 4.4 4.5 4.6 4.7
+	5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.7
+	6.0 6.1 6.2 6.3 6.4 6.5 6.6 6.7
+	7.0 7.1 7.2 7.3 7.4 7.5 7.6 7.7
+*/
 #include <iostream>
+#include <cctype>
 
 using namespace std;
 
+// --------------------------------------------------------------------------------------
+
 class Piece{
 	public:
-		Piece(){}
-		Piece(const std::string n, string c, size_t x, size_t y):mName(n),mX(x),mY(y){
-			if(c == "white"){mColor = false;}
+		Piece(){
+			mName;
+			mColor;
+			mAlive;
+			mInitial;
 		}
 		~Piece(){}
+	
+		void setPiece(const std::string n, string c){
+			mName = n;
+			if(c == "white"){mColor = false;}
+			else if(c == "black"){mColor = true;}
+			
+			if(n == "Pawn") {
+				if(mColor == false) mInitial ='p';
+				else if(mColor == true) mInitial='P';}
+			else if(n == "Knight") {
+				if(mColor == false) mInitial='n';
+				else if(mColor == true) mInitial='N';}
+			else if(n == "Bishop") {
+				if(mColor == false) mInitial='b';
+				else if(mColor == true) mInitial='B';}
+			else if(n == "Rook") {
+				if(mColor == false) mInitial='r';
+				else if(mColor == true) mInitial='R';}
+			else if(n == "Queen") {
+				if(mColor == false) mInitial='q';
+				else if(mColor == true) mInitial='Q';}
+			else if(n == "King") {
+				if(mColor == false) mInitial='k';
+				else if(mColor == true) mInitial='K';}
+		}
 	
 		void setMName(){
 			cout << "Piece Name : ";
 			cin >> mName;
 		}
+		void setMName(string name){
+			mName = name;
+		}
 		string getMName(){
 			return mName;
 		}
 	
-		void setMColor(){
-			string color;
-			cout << "black or white :";
-			cin >> color;
+		void setMColor(string color){
 			if(color == "white"){mColor = false;}
+			else if(color == "black"){mColor = true;}
 		}
 		bool getMColor(){
 			return mColor;
 		}
-	
-		void setMX(){
-			cout << "x : ";
-			cin >> mX;
-		}
-		int getMX(){
-			return mX;
-		}	
-	
-		void setMY(){
-			cout << "y : ";
-			cin >> mY;
-		}
-		int getMY(){
-			return mY;
+		
+		char getMInitial(){
+			return mInitial;
 		}
 	
 		void dead(){
@@ -58,69 +88,111 @@ class Piece{
 		string mName;
 		bool mColor = true; // black : true, white : false
 		bool mAlive = true;
-		size_t mX;
-		size_t mY;
+		char mInitial;
 };
+
 
 // --------------------------------------------------------------------------------------
-// 여기서부터
-class Pawn : public Piece{
-	public:
-		Pawn(const std::string n, string c, size_t x, size_t y):Piece(n,c,x,y){
-			if(c == "white"){mColor = false;}
-		}
-	
-		void setMPromote(){
-			mPromte = true;
-		}
-		bool getMPromote(){
-			return mPromte;
-		}
-	
-	private:
-		bool mPromte = false;
-};
 
-class Board{
+
+// --------------------------------------------------------------------------------------
+
+class Board: public Piece {
 	public:
 		Board(){}
-		Board(int x, int y):col(x), row(y){
-			mBoardArr = new char* [col];
-			for(int i=0;i<col;i++){
-				mBoardArr[i] = new char[row];
-				for(int j=0;j<row;j++){
-					mBoardArr[i][j] = {'o'};
-				}
-			}
-		}
 		~Board(){
 			for(int i=0;i<col;i++){
 				delete [] mBoardArr[i];
 			}
 			delete [] mBoardArr;
 		}
-	
-	
-		void setMBoardArr(){
-			cout << "X : "; cin >> col;
-			cout << "Y : "; cin >> row;
+		
+		void makeMBoardArr(int x, int y){
+			col = x;
+			row = y;
 			
 			mBoardArr = new char* [col];
 			for(int i=0;i<col;i++){
-				mBoardArr[i] = new char[row];
+				mBoardArr[i] = new char [row];
 				for(int j=0;j<row;j++){
+					/*if (i==0 && j == 0){
+						mBoardArr[i][j] = &piece[8];
+					}
+					else if (i==0 && j == 1){
+						mBoardArr[i][j] = &piece[1];
+					}
+					else if(i == 1){
+						for(int k = 0; k<8;k++)
+						mBoardArr[i][j] = &piece[k];} */
 					mBoardArr[i][j] = {'o'};
 				}
 			}
+			/*
+			mBoardArr[0][8] = '8';
+			mBoardArr[1][8] = '7';
+			mBoardArr[2][8] = '6';
+			mBoardArr[3][8] = '5';
+			mBoardArr[4][8] = '4';
+			mBoardArr[5][8] = '3';
+			mBoardArr[6][8] = '2';
+			mBoardArr[7][8] = '1';
+			mBoardArr[8][0] = 'a';
+			mBoardArr[8][1] = 'b';
+			mBoardArr[8][2] = 'c';
+			mBoardArr[8][3] = 'd';
+			mBoardArr[8][4] = 'e';
+			mBoardArr[8][5] = 'f';
+			mBoardArr[8][6] = 'g';
+			mBoardArr[8][7] = 'h';
+			mBoardArr[8][8] = ' ';*/
+		}
+		void initMBoardArr(){
+			mBoardArr[1][0] = 'P'; // black
+			mBoardArr[1][1] = 'P';
+			mBoardArr[1][2] = 'P';
+			mBoardArr[1][3] = 'P';
+			mBoardArr[1][4] = 'P';
+			mBoardArr[1][5] = 'P';
+			mBoardArr[1][6] = 'P';
+			mBoardArr[1][7] = 'P'; 
+			mBoardArr[0][0] = 'R'; 
+			mBoardArr[0][1] = 'N';
+			mBoardArr[0][2] = 'B';
+			mBoardArr[0][3] = 'Q';
+			mBoardArr[0][4] = 'K';
+			mBoardArr[0][5] = 'B';
+			mBoardArr[0][6] = 'N';
+			mBoardArr[0][7] = 'R';
+			
+			mBoardArr[6][1] = 'p'; // white
+			mBoardArr[6][2] = 'p';
+			mBoardArr[6][3] = 'p';
+			mBoardArr[6][4] = 'p';
+			mBoardArr[6][5] = 'p';
+			mBoardArr[6][6] = 'p';
+			mBoardArr[6][7] = 'p'; 
+			mBoardArr[7][0] = 'r'; 
+			mBoardArr[7][1] = 'n';
+			mBoardArr[7][2] = 'b';
+			mBoardArr[7][3] = 'q';
+			mBoardArr[7][4] = 'k';
+			mBoardArr[7][5] = 'b';
+			mBoardArr[7][6] = 'n';
+			mBoardArr[7][7] = 'r';
+		}
+		void pieceSet(int x, int y, char i){  // 기물의 이니셜(i)로 클래스 파악 후 x y 위치로 이동
+			
 		}
 		char** getMBoardArr(){
 			return mBoardArr;
 		}
+		
+		
 	
 		void boardview(){
 			for(int i=0;i<col;i++){
 				for(int j=0;j<row;j++){	
-					cout << mBoardArr[i][j] << " ";
+					cout << mBoardArr[i][j] <<" ";
 				} cout << endl;
 			} cout << endl;
 		}
@@ -128,16 +200,22 @@ class Board{
 		int col; //x
 		int row; //y
 		char** mBoardArr;
+		Piece piece[32];
 };
 
-int main(){
-	cout << "Piece" << endl;
-	Pawn p("pawn","black",1,0);
-	cout << "이름 : " << p.getMName() << endl
-		 << "색깔(1=검정, 2=흰색) : " << p.getMColor() << endl
-		 << "X : " << p.getMX() << " Y : " << p.getMY() << endl;
+// --------------------------------------------------------------------------------------
+
+class game : public Board{
 	
-	cout << "Board" << endl;
-	Board b(8,8);
+};
+// --------------------------------------------------------------------------------------
+
+
+
+int main(){
+	int turn=0;
+	Board b;
+	b.makeMBoardArr(8,8);
+	b.initMBoardArr();
 	b.boardview();
 }
